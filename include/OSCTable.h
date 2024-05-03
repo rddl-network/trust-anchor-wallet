3c66c5177433d22b596c89f78d38d5efbeef47b6
fasagidagrc <fatih.asagidag@riddleandcode.com>
2024-05-03 11:53:31 +0300

    se050 functions added

--- include/OSCTable.h
+++ include/OSCTable.h
@@ -6,7 +6,10 @@ constexpr std::pair<const char*, void(*)(OSCMessage&, int)> osc_func_table[] = {
 #ifdef DSE050
     {"/IHW/se050SetSeed", &routeSe050SetSeed},
     {"/IHW/se050GetSeed", &routeSe050GetSeed},
+    {"/IHW/se050CreateKeyPair", &routeSe050CreateKeyPair},
     {"/IHW/se050CalculateHash", &routeSe050CalculateHash},
+    {"/IHW/se050SignData", &routeSe050SignData},
+    {"/IHW/se050VerifySignature", &routeSe050VerifySignature},
 #endif
 #ifdef DOPTIGA
     {"/IHW/optigaTrustXCreateSecret", &routeOptigaTrustXCreateSecret},
--- include/se050Handler.h
+++ include/se050Handler.h
@@ -9,6 +9,7 @@ void routeSE050EncryptData(OSCMessage &msg, int addressOffset);
 void routeSE050DecryptData(OSCMessage &msg, int addressOffset);
 void routeSe050SetSeed(OSCMessage &msg, int addressOffset);
 void routeSe050GetSeed(OSCMessage &msg, int addressOffset);
+void routeSe050CreateKeyPair(OSCMessage &msg, int addressOffset);
 void routeSe050CalculateHash(OSCMessage &msg, int addressOffset);
 void routeSe050SignData(OSCMessage &msg, int addressOffset);
 void routeSe050VerifySignature(OSCMessage &msg, int addressOffset);
